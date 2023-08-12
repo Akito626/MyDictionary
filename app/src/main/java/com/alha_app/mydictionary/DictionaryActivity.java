dec3b8be0ad429e5de43a5652bf7d3f825f8fe3f
Akito <al6.vha12@gmail.com>
2023-08-12 13:55:56 +0900

    ホームからListをタップして辞書画面に移動できるようにしました。データの受け渡しも出来るようにしました。

--- app/src/main/java/com/alha_app/mydictionary/DictionaryActivity.java
+++ app/src/main/java/com/alha_app/mydictionary/DictionaryActivity.java
@@ -1,14 +1,32 @@
 package com.alha_app.mydictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class DictionaryActivity extends AppCompatActivity {
+    private MyDictionary myDictionary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+
+        myDictionary = (MyDictionary) this.getApplication();
+
+        Toolbar toolbar = findViewById(R.id.toolbar_dictionary);
+        toolbar.setTitle(myDictionary.getTitle());
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/alha_app/mydictionary/MyDictionary.java
@@ -0,0 +1,35 @@
+package com.alha_app.mydictionary;
+
+import android.app.Application;
+
+public class MyDictionary extends Application {
+    private int id;
+    private String title;
+    private String detail;
+
+    // Getter
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    // Setter
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
